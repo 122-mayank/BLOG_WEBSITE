94ef05913a076f66b89e207ddcacc3d7d829a319
MAYANK SAINI <Mayank Saini>
2025-11-15 15:53:37 +0530

    Adding files

---
+++ public/script/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine','ejs');
+app.set('views','../views');
+
+app.use(express.static(path.join(__dirname,'../style')));
+app.use(express.static(path.join(__dirname,'../media')));
+
+app.get('/',(req,res)=>{
+       res.render("index");
+});
+
+app.get('/food_blog',(req,res)=>{
+    res.render("food_blog");
+});
+
+app.get('/signin',(req,res)=>{
+    res.render('signin');
+});
+
+app.get('/adventure_blog',(req,res)=>{
+    res.render('adventure_blog');
+});
+
+app.get('/notes_blog',(req,res)=>{
+     res.render('notes_blog');
+});
+
+app.get('/office_blog',(req,res)=>{
+     res.render('office_blog');
+});
+
+app.get('/blog',(req,res)=>{
+    res.render('index');
+});
+
+
+
+const PORT = 3001;
+app.listen(PORT,()=>{
+        console.log(`Server is running on http://localhost:${PORT}`);
+});
